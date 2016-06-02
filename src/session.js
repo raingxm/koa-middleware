28360bcf6f16dc87efe08620421e0c37572cd5a7
zhangxu <raingxm@163.com>
2016-06-02 23:22:06 +0800

    pass test, able to retrieve name

--- src/session.js
+++ src/session.js
@@ -1,6 +1,35 @@
+var _ = require('lodash'),
+  uid = require('uid-safe');
+
 module.exports = function(option) {
+  var store = {};
+  var cookieName = 'koa.sid';
+  var cookieOption = {
+    httpOnly: true,
+    path: '/',
+    overwrite: true,
+    signed: true,
+    maxAge: 24 * 60 * 60 * 1000
+  };
+
   return function *session(next) {
-    this.session = {};
+    var token = this.cookies.get(cookieName);
+    if(token && _.has(store, token)) {
+      this.session = store[token];
+    }
+
+    if(!this.session) {
+      this.session = {};  
+    }
+    
     yield next;
+
+    if(!token) {
+      token = uid(24);
+      this.cookies.set(cookieName, token, cookieOption);
+      if(this.session) {
+        store[token] = this.session;
+      }
+    }
   }
 };
